6bcad2604d5d27b5903128dd57a7071f33b0ae10
mevlut <mevlutkaymaz@gmail.com>
2022-10-08 17:43:05 -0500

    more tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -3,9 +3,14 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndexControllerTest {
@@ -19,6 +24,8 @@ class IndexControllerTest {
     void index() {
         assertEquals("index", indexController.index());
         assertEquals("index", indexController.index(), "Wrong view returned");
+
+        assertThat(indexController.index().equals("index"));
     }
 
     @Test
@@ -52,5 +59,25 @@ class IndexControllerTest {
             Thread.sleep(5000);
             System.out.println("I got here2");
         });
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testOnWindows() {
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testOnMac() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testJava8() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testJava11() {
     }
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnerTest {
@@ -26,7 +28,7 @@ class OwnerTest {
                     });
                 }
                 );
-
+        assertThat(owner.getCity(), is("Spring"));
     }
 
 }
